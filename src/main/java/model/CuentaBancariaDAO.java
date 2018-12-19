168c640e6345cbded73e48ea05999089df14d774
Carolyns-Lagos <carolyns.echegaray@gmail.com>
2018-12-19 15:48:24 -0300

    Ejercicios clase 19-12-18

--- src/main/java/model/CuentaBancariaDAO.java
+++ src/main/java/model/CuentaBancariaDAO.java
@@ -4,6 +4,7 @@ import exepciones.SinConexionException;
 
 import javax.swing.plaf.nimbus.State;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -19,12 +20,33 @@ public class CuentaBancariaDAO {
 
     public void insertarCuenta(CuentaBancaria o) throws SinConexionException {
         try{
-            psInsert= conexion.getConexion().prepareStatement("");
+            psInsert= conexion.getConexion().
+                    prepareStatement("INSERT INTO cuentabancaria(idCuenta,SaldoDisponible,TipoCuenta)"+"Vaue(?,?,?)");
+            psInsert.setString(1,o.getIdCuenta());
+            psInsert.setInt(2,o.getSaldoDisponible());
+            psInsert.setString(3,o.getTipoCuenta());
+            psInsert.executeUpdate(); //Es cualquier cambio que agregue o modifique algo, con esto de ejecuta
         }catch (SQLException ex){
             System.out.println("Error"+ex);
             //Mostramos solo el error
         }
+    }
 
+    public void obtenerCuenta(){
+        try{
+            psInsert=conexion.getConexion().prepareStatement("SELECT * FROM cuentabanco");
+            ResultSet rs=psInsert.executeQuery();//Este quiery siempre se guarda en el result set
+
+            while (rs.next()){
+                System.out.println(rs.getString("idCuenta")+ " | ");
+                System.out.println(rs.getString("SaldoDisponible")+ " | ");
+                System.out.println(rs.getString("TopoCuenta")+ " | ");
+            }
+
+        }catch (SQLException e) {
+            System.out.println("Error" + e);
+        }
 
     }
+
 }
